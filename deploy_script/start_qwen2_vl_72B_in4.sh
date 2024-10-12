72f23fb3bad5c7ed9b94fb6bf8b1d69ea87fa776
tzm_remote <you@example.com>
2024-10-12 12:54:03 +0800

    add: multi image support

--- deploy_script/start_qwen2_vl_72B_in4.sh
+++ deploy_script/start_qwen2_vl_72B_in4.sh
@@ -8,9 +8,7 @@ unset https_proxy
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 vllm serve /models/vlm/qwen/Qwen2-VL-72B-Instruct-GPTQ-Int4 \
 --served-model-name Qwen/Qwen2-VL-72B-Instruct-GPTQ-Int4 \
---pipeline-parallel-size 4 \
+--tensor-parallel-size 4 \
 --host 0.0.0.0 \
 --port 8023 \
---gpu-memory-utilization 0.95 \
---max-model-len 4096
-
+--limit-mm-per-prompt image=24
--- deploy_script/start_qwen2_vl_7B.sh
+++ deploy_script/start_qwen2_vl_7B.sh
@@ -7,8 +7,11 @@ unset http_proxy
 unset https_proxy
 
 CUDA_VISIBLE_DEVICES=0 vllm serve /models/vlm/qwen/Qwen2-VL-7B-Instruct \
---served-model-name Qwen/Qwen2-VL-7B-Instruct  \
+--served-model-name Qwen/Qwen2-VL-7B-Instruct \
 --host 0.0.0.0 \
---port 8022 \
---max-model-len 7920 \
---gpu-memory-utilization 0.95
+--port 8012 \
+--max-num-seqs 16 \
+--gpu-memory-utilization 0.99 \
+--max-model-len 16384 \
+--limit-mm-per-prompt image=12
+
