490032d63d89d7d02afbe2870ef9c4de832d258f
tzm_remote <you@example.com>
2024-10-14 08:49:01 +0800

    add: test cases

---
+++ test/emb_api_test.py
@@ -0,0 +1,26 @@
+import time
+from openai import OpenAI
+
+openai_api_key = "EMPTY"
+openai_api_base = "http://10.23.56.12/v1"
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+t1 = time.time()
+models = client.models.list()
+model = models.data[0].id
+
+# # test emb
+response = client.embeddings.create(
+    input="The food was delicious and the waiter...",
+    model="Alibaba-NLP/gte-Qwen2-1.5B-instruct",
+    encoding_format="float"
+)
+
+print(response)
+print(time.time() - t1)
+
---
+++ test/text_api_test.py
@@ -0,0 +1,50 @@
+import time
+from openai import OpenAI
+
+# Modify OpenAI's API key and API base to use vLLM's API server.
+openai_api_key = "EMPTY"
+openai_api_base = "http://10.23.56.12/v1"
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+
+t1 = time.time()
+models = client.models.list()
+# model = models.data[0].id
+model = "Qwen/Qwen2_5-72B-Instruct-GPTQ-Int4"
+
+# # test chat
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "Who are you?"
+        }
+    ],
+    model=model,
+    max_tokens=64,
+)
+
+result = chat_completion.choices[0].message.content
+print("Chat completion output:", result)
+t2 = time.time()
+print(t2 - t1)
+
+# test completion
+completion = client.completions.create(
+    model="Qwen/Qwen2_5-72B-Instruct-GPTQ-Int4",
+    prompt="San Francisco is a",
+    max_tokens=7,
+    temperature=0
+)
+
+result = completion.choices[0].text
+print("Completion output:", result)
+print(time.time() - t2)
