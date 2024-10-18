a1e2c9827750be36e0f00a535a7182b335ecea62
tzm_remote <you@example.com>
2024-10-18 12:35:07 +0800

    change: deployment gpu id

--- deploy_script/start_qwen2_5_72B_in4_2gpu.sh
+++ deploy_script/start_qwen2_5_72B_in4_2gpu.sh
@@ -6,7 +6,7 @@ conda activate vllm-infer
 unset http_proxy
 unset https_proxy
 
-CUDA_VISIBLE_DEVICES=2,3 vllm serve /models/llm/Qwen/Qwen2___5-72B-Instruct-GPTQ-Int4 \
+CUDA_VISIBLE_DEVICES=1,2 vllm serve /models/llm/Qwen/Qwen2___5-72B-Instruct-GPTQ-Int4 \
 --host 0.0.0.0 \
 --port 8012 \
 --served-model-name Qwen/LLM \
